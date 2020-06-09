dd19c9926710b792874c97bcb1a5bb9e2cc8ffa8
joascorrea96 <57044157+joascorrea96@users.noreply.github.com>
2020-06-09 01:18:30 -0300

    Update index.js

--- src/app/controllers/index.js
+++ src/app/controllers/index.js
@@ -2,14 +2,8 @@ const fs = require('fs')
 const path = require('path')
 
 module.exports = app => {
-    /*comando para ler o diretório atual*/
     fs
     .readdirSync(__dirname)
-    /*irá buscar todos os arquivos da pasta atual
-    que não iniciam com ponto, e não são do tipo
-    'index.js'.*/
     .filter(file => ((file.indexOf('.')) !== 0 && (file !== "index.js")))
-    /*Percorrá os arquivos, importando cada um deles 
-    passando o app para da um.*/
     .forEach(file => require(path.resolve(__dirname, file))(app))
 }

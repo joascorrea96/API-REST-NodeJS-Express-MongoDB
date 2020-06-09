0065f7665de81eade3acd1ffdd7f864999a4a356
joascorrea96 <57044157+joascorrea96@users.noreply.github.com>
2020-06-09 01:24:14 -0300

    Update index.js

--- src/database/index.js
+++ src/database/index.js
@@ -1,9 +1,5 @@
-//importação do 'mongoose' para manipular o banco
 const mongoose = require('mongoose')
 
-/*aqui é criado um banco de dados chamado 'noderest'
-passando como segundo parâmetro alguns comandos que vão servir
-para realizar a conexão com o mongo*/
 mongoose.connect("mongodb://localhost/noderest", {
   useNewUrlParser: true,
   useUnifiedTopology: true,
@@ -11,9 +7,6 @@ mongoose.connect("mongodb://localhost/noderest", {
 });
 
 
-//define qual a classe a ser utilizada pelo mongoose
 mongoose.Promise = global.Promise
 
-
-//aqui será exportado a constante 'mongoose'
 module.exports = mongoose
